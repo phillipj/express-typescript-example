283153f3add2fb41cb6fd7dba93d4c3c167294b8
Phillip Johnsen <phillip.johnsen@tomra.com>
2019-04-24 21:10:41 +0200

    feat: add /echo endpoint that grabs an URL query parameter
    ..also written intentially to get a little blipp of `undefined` demoed,
    just in case we've got time to demo a sound language like ReasonML
    in addition and how that fixes it.

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,12 @@ app.get("/", (request, response) => {
   response.send("Vrrooooom! 🚙💨");
 });
 
+app.get("/echo", (request, response) => {
+  const name = request.query.name;
+
+  response.send(`Hello there ${name}!`);
+});
+
 app.listen(port, (err?: Error) => {
   if (err) {
     return console.error("Yiikes something exploded:", err);
